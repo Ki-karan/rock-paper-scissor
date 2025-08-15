2c8e1e68e4c8fffcea9a3274e55a63b278d6b6f4
Ki-karan <207749107+Ki-karan@users.noreply.github.com>
2025-08-15 20:28:20 +0530

    add score tracking and fix game logic add winner/looser messages

--- script.js
+++ script.js
@@ -3,20 +3,47 @@ function getHumanChoice() {
     return choice;
 }
 
-console.log(getHumanChoice())
+
 
 function getComputerChoice() {
     let randomNum = Math.random();
 
     if ( randomNum < 0.34 ) {
         return "rock";  
-    } else if (randomNum > 0.59 ) {
+    } else if (randomNum > 0.67 ) {
         return "paper";
     } else { 
         return "scissors";
     }
     
 }
-console.log(getComputerChoice());
+
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, ComputerChoice) {
+    console.log(humanChoice)
+    console.log(ComputerChoice)
+   
+    if (humanChoice === ComputerChoice) {
+        return "It's a tie! Both chose " + humanChoice;
+    } else if ((humanChoice === "scissors" && ComputerChoice === "paper") ||
+            (humanChoice === "rock" && ComputerChoice === "scissors") ||
+            (humanChoice === "paper" && ComputerChoice === "rock")) {
+        humanScore++;
+        return `You Win! ${humanChoice} beats ${ComputerChoice}`;;
+    } else {
+        computerScore++;
+        return `You lose! ${ComputerChoice} beats ${humanChoice}`;
+    }
+
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+console.log(playRound(humanSelection, computerSelection));
+console.log(`Your score: ${humanScore}`);
+console.log(`Computer score: ${computerScore}`);
 
 
